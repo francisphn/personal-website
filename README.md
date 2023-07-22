f1890bec31ce537fc526edf0f41499bc49df5758
Francis Phan <danny.phan2004@gmail.com>
2023-07-23 11:38:51 +1200

    Format with Prettier

--- README.md
+++ README.md
@@ -9,4 +9,3 @@ First, clone the repository, then run `pnpm run dev`.
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`.
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,20 @@ const Home = () => {
     <div className="p-16 space-y-4">
       <div className="">
         <p className="font-bold text-xl">Hi! My name is Francis Phan</p>
-        <p>I&apos;m a Computer Science student at the University of Canterbury.</p>
+        <p>
+          I&apos;m a Computer Science student at the University of Canterbury.
+        </p>
         <p>I currently work for Triage-Plus, a health startup. Once a Xero.</p>
       </div>
 
       <div>
-        <p className='font-bold'>My current interests:</p>
+        <p className="font-bold">My current interests:</p>
 
         <ul className="list-disc pl-6 space-y-1.5">
-          <li>Distributed and cluster computing (Kubernetes, Google F1, Apache Spark)</li>
+          <li>
+            Distributed and cluster computing (Kubernetes, Google F1, Apache
+            Spark)
+          </li>
           <li>Parallelism and scalability</li>
           <li>Content-based recommendation systems</li>
           <li>CI/CD, CI/CD optimisation and platform observability</li>
@@ -26,18 +31,24 @@ const Home = () => {
         <p className="font-bold">Email me</p>
         <p>francis [at] phan.nz</p>
       </div>
-        
+
       <div className="flex space-x-4">
-        <Link href="https://drive.google.com/file/d/1JoQNDuJzrMtdsxmcsa42CdjgPgSnjzAK/view?usp=sharing" className="underline underline-offset-2 text-blue-700 hover:text-black">
+        <Link
+          href="https://drive.google.com/file/d/1JoQNDuJzrMtdsxmcsa42CdjgPgSnjzAK/view?usp=sharing"
+          className="underline underline-offset-2 text-blue-700 hover:text-black"
+        >
           CV
         </Link>
 
-        <Link href="https://github.com/francisphn" className="underline underline-offset-2 text-blue-700 hover:text-black">
+        <Link
+          href="https://github.com/francisphn"
+          className="underline underline-offset-2 text-blue-700 hover:text-black"
+        >
           GitHub
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
